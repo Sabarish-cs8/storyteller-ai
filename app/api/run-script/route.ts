1f52a8f661fb41ef73992bea219eea8b39053702
sabarish <sabarish.cs8@gmail.com>
2024-12-12 15:59:40 +0530

    Add gptScriptInstance

--- app/api/run-script/route.ts
+++ app/api/run-script/route.ts
@@ -1,5 +1,9 @@
+import g from "@/lib/gptScriptInstance";
 import { RunOpts } from "@gptscript-ai/gptscript";
 import { NextRequest } from "next/server";
+
+
+const script = "app/api/run-script/story-book.gtp";
 
 export async function POST(request:NextRequest){
     const {story,pages,path}= await request.json();
@@ -8,5 +12,23 @@ export async function POST(request:NextRequest){
     const opts:RunOpts={
         disableCache:true,
         input:`--story ${story} --pages ${pages} --path${path}`,
+    };
+
+    try {
+        const encoder=new TextEncoder();
+        const stream = new ReadableStream({
+            async start(controller){
+                try {
+                    const run = await g.run(script,opts);
+                } catch (error) {
+                  controller.error(error)
+                  console.error("Error:",error)                   
+                }
+            }
+        })
+    } catch (error) {
+        return new Response(JSON.stringify({error:error}),{
+            status:500,
+        });
     }
 }
