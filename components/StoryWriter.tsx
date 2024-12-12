57209bc0b801750132fbaf5850904891703fcb06
sabarish <sabarish.cs8@gmail.com>
2024-12-12 14:00:06 +0530

    Add route

--- components/StoryWriter.tsx
+++ components/StoryWriter.tsx
@@ -5,9 +5,40 @@ import { Button } from "./ui/button"
 import { Select, SelectTrigger, SelectValue,SelectContent, SelectItem } from "./ui/select"
 import { Textarea } from "./ui/textarea"
 
+const storiesPath = "public/stories";
+
 
 function StoryWriter() {
-    const [story,setStory]=useState<String>("");
+    const [story,setStory]=useState<string>("");
+    const [pages,setPages]=useState<number>();
+    const [progress,setProgress]=useState("");
+    const [runStarted,setRunStarted]=useState<boolean>(false);
+    const [runFinished,setRunFinished]=useState<boolean | null>(null);
+    const [currentTool,setCurrentTool]=useState("");
+
+    async function runScript(){
+        setRunStarted(true);
+     setRunFinished(false); 
+     
+     const response = await fetch("/api/run-script",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json",
+        },
+        body:JSON.stringify({story,pages,path:storiesPath}),
+     });
+
+     if(response.ok && response.body){
+        //handle streams from the API
+        //...
+        console.log("Streaming started");
+        //...
+     }else{
+        setRunFinished(true);
+        setRunStarted(false);
+        console.error("Failed to start streaming");
+     }
+    }
   return (
     <div className="flex flex-col container">
         <section className="flex-1 flex flex-col border border-purple-300 rounded-md p-10 space-y-2">
@@ -30,13 +61,45 @@ function StoryWriter() {
                     ))}
                 </SelectContent>
             </Select>
-            <Button disabled={!story || !pages } 
-            className="w-full" size="lg">
+            <Button disabled={!story || !pages || runStarted } 
+            className="w-full" size="lg"
+            onClick={runScript}
+            >
                 Generate Story
             </Button>
         </section>
 
-        <section className="flex-1 pb-5 mt-5"></section>
+        <section className="flex-1 pb-5 mt-5">
+            <div className="flex flex-col-reverse w-full space-y-2 bg-gray-800 rounded-md text-gray-200 font-mono p-10 h-96 overflow-y-auto">
+                <div>
+                    {runFinished === null &&(
+                        <>
+                        <p className="animate-pulse mr-5">Im waiting for you to Generate a story above...</p>
+                        </>
+                    )}
+                    <span className="mr-5">{">>"}</span>
+                    {/**progress */}
+                    {progress}
+                </div>
+                {/**Current Tool */}
+                {currentTool && (
+                    <div className="py-10">
+                        <span className="mr-5">{"---[Current Tool]---"}</span>
+                        {currentTool}
+                    </div>
+                )}
+
+                {/**Render Events... */}
+                {runStarted && (
+                    <div>
+                        <span className="mr-5 animate-in">
+                            {"---[AI Storyteller Has Started]---"}
+                        </span>
+                        <br />
+                    </div>
+                )}
+            </div>
+        </section>
     </div>
   )
 }
