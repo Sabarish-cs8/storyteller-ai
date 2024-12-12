1e86acf9bdb4421aa56cfb0e9f991c89fa9b5bcf
sabarish <sabarish.cs8@gmail.com>
2024-12-12 13:01:30 +0530

    Add StoryWriter page

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="flex flex-col min-h-screen">
         {/**Header */}
         <Header />
 
