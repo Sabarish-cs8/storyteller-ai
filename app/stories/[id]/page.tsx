54fe3f8bd40163f0a1828c30b9cdddb431d3e2a6
sabarish <sabarish.cs8@gmail.com>
2024-12-14 13:32:38 +0530

    Error Fixed

--- app/stories/[id]/page.tsx
+++ app/stories/[id]/page.tsx
@@ -4,28 +4,34 @@ import { notFound } from "next/navigation";
 
 
 interface StoryPageProps{
-    params:{
+    params: Promise<{
         id:string;
+    }>
+}
+async function StoryPage(props: StoryPageProps) {
+    const params = await props.params;
+
+    const {
+        id
+    } = params;
+
+    // Explanation: The id is URL encoded, so we need to decode it before using it to get the story. This fixes the issue where the story is not founded when the id contains special characters such as %20 for spaces.
+    const decodedId = decodeURIComponent(id);
+
+    const story = await getStory(decodedId);
+
+    if(!story){
+        return notFound();
     }
-}
-function StoryPage({params:{id}} : StoryPageProps) {
-// Explanation: The id is URL encoded, so we need to decode it before using it to get the story. This fixes the issue where the story is not founded when the id contains special characters such as %20 for spaces.
-const decodedId = decodeURIComponent(id);
-
-const story = getStory(decodedId);
-
-if(!story){
-    return notFound();
-}
-  return (
-    <Story story={story} />
-  )
+    return (
+      <Story story={story} />
+    )
 }
 
 export default StoryPage
 
-export async function generateStaticPArams(){
-    const stories = getAllStories();
+export async function generateStaticParams(){
+    const stories =await getAllStories();
 
     //Explanation : We need to return an array of objects with the id property set to the story id. This will generate a static page for each story.
     //Example : [{id:"story-1"},{id:"story-2"},...]
